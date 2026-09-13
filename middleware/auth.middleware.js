e0d1eaf97671ebdcc1cce600bf402f0e824c4429
Oyetomi Oyelaja <torreyjh@gmail.com>
2026-09-13 21:27:12 +0200

    Make user creation api require authentication again

--- middleware/auth.middleware.js
+++ middleware/auth.middleware.js
@@ -3,9 +3,8 @@ const jwt = require("jsonwebtoken");
 function authMiddleware(req, res, next) {
   // Public route - login does not require authentication
   if (
-  req.path === "/api/auth/login" ||
-  (req.path === "/api/users" && req.method === "POST") // temporarily make this public in order to add an admin user.
-  ) {
+  req.path === "/api/auth/login")
+  {
     return next();
   }
 
